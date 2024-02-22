905304a4d0f4a1f7b8ff673710d1e762feda5429
郑震 <2693781894@qq.com>
2024-02-22 22:13:39 +0800

    impl communicate between reactNative with Unity webGL

--- tsExe/App.tsx
+++ tsExe/App.tsx
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Button, Dimensions, StyleSheet, Text, View } from 'react-native';
 import { Unity ,useUnityContext} from 'react-unity-webgl';
 import WebView from 'react-native-webview';
+import React, { createRef, useRef } from 'react';
 //require("./assets/release/index.html")
 const localWebGL = require("./assets/release/index.html")
 export default function App() {
+  const [fromUnity,setMessageFromUniyt] = React.useState('')
   
+
+
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  let   webview:any = null
+  const toUnity = ()=>{
+      //alert("1")
+      webview.postMessage("toUnity")
+  }
+  
+const  handleFromUnity = (str:string)=>{
+    setMessageFromUniyt(str)
+  }
   return (
     <View style={styles.webView}>
       <WebView 
       source={localWebGL}
-      onMessage={(e)=>{console.log(e.nativeEvent.data)}}/>
+      onMessage={(e)=>{handleFromUnity(e.nativeEvent.data)}}
+      ref={(ref)=>{webview=ref}}/>
+
+      
+      <Text>{`I am react native lable,this is message from unity:   ${fromUnity}`}</Text>
+      <Button title='React Native Button to rotate cube'
+      onPress={toUnity}/>
     </View>
   );
 }
@@ -24,6 +45,6 @@ const styles = StyleSheet.create({
   },
   webView:{
     width:Dimensions.get('window').width,
-    height:Dimensions.get("window").height
+    height:Dimensions.get("window").height/1.3
   }
 });
